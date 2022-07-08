2c775b0c5a7010b7274df5d7551bc81d597d170d
TheerthaPraveen <theerthapraveen.n@gmail.com>
2022-07-08 11:35:32 +0530

    stud

--- src/app/view-student/view-student.component.ts
+++ src/app/view-student/view-student.component.ts
@@ -11,5 +11,17 @@ export class ViewStudentComponent implements OnInit {
 
   ngOnInit(): void {
   }
-name="akhil"
+data=[
+
+{"name":"Rahul","age":45,"photo":"https://i.pinimg.com/736x/b1/88/c6/b188c6801ad1d71d3c962c6e4aa2d0cf.jpg"},
+{"name":"Ritha","age":35,"photo":"https://i.pinimg.com/736x/b1/88/c6/b188c6801ad1d71d3c962c6e4aa2d0cf.jpg"},
+{"name":"Gauri","age":20,"photo":"https://i.pinimg.com/736x/b1/88/c6/b188c6801ad1d71d3c962c6e4aa2d0cf.jpg"},
+{"name":"Sruthy","age":19,"photo":"https://i.pinimg.com/736x/b1/88/c6/b188c6801ad1d71d3c962c6e4aa2d0cf.jpg"},
+{"name":"Sree","age":21,"photo":"https://i.pinimg.com/736x/b1/88/c6/b188c6801ad1d71d3c962c6e4aa2d0cf.jpg"},
+{"name":"Nivedhya","age":22,"photo":"https://i.pinimg.com/736x/b1/88/c6/b188c6801ad1d71d3c962c6e4aa2d0cf.jpg"},
+{"name":"Abhi","age":25,"photo":"https://i.pinimg.com/736x/b1/88/c6/b188c6801ad1d71d3c962c6e4aa2d0cf.jpg"}
+
+
+
+]
 }
